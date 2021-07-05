51bce35e17a1e6405926dd736e5f4c2d631b1d6d
Silfryi Kalsandryn <silfryikalsandryn@protonmail.com>
2021-07-05 10:27:01 -0400

    Make tower output at 200 mB/t not earlier value

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/TileEntityDistillationTower.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/TileEntityDistillationTower.java
@@ -212,7 +212,7 @@ public class TileEntityDistillationTower extends TileEntityMultiblockMetal<TileE
 				lastFluidOut = null;
 				if (targetFluidStack != null)
 				{
-					FluidStack out = Utils.copyFluidStackWithAmount(targetFluidStack, Math.min(targetFluidStack.amount, 100), false);
+					FluidStack out = Utils.copyFluidStackWithAmount(targetFluidStack, Math.min(targetFluidStack.amount, 200), false);
 					int accepted = output.fill(out, false);
 					if (accepted > 0)
 					{
@@ -227,7 +227,7 @@ public class TileEntityDistillationTower extends TileEntityMultiblockMetal<TileE
 						while (iterator.hasNext())
 						{
 							targetFluidStack = iterator.next();
-							out = Utils.copyFluidStackWithAmount(targetFluidStack, Math.min(targetFluidStack.amount, 100), false);
+							out = Utils.copyFluidStackWithAmount(targetFluidStack, Math.min(targetFluidStack.amount, 200), false);
 							accepted = output.fill(out, false);
 							if (accepted > 0)
 							{
