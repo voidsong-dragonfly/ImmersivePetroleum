e01c593692b731ebebe71ba050b963ba1c20db46
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2022-06-09 20:21:21 +0200

    Clone of c2beb29d713118fb92277043707f137de695aedc

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/GasGeneratorTileEntity.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/GasGeneratorTileEntity.java
@@ -39,6 +39,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -374,5 +375,10 @@ public class GasGeneratorTileEntity extends ImmersiveConnectableBlockEntity impl
 		float xo = facing.getNormal().getX() * .5f + .5f;
 		float zo = facing.getNormal().getZ() * .5f + .5f;
 		return new Vec3(xo, .5f, zo);
+	}
+	
+	@Override
+	public Collection<ResourceLocation> getRequestedHandlers(){
+		return ImmutableList.of(EnergyTransferHandler.ID);
 	}
 }
