7f0b3691968aed751af4933a52eb85903bec04c2
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2022-07-25 18:10:53 +0200

    Bump IE to `1.18.2-8.1.0-pre.32`
    And removing `IEItemInterfaces.IColouredItem` from IPItemBase

--- src/main/java/flaxbeard/immersivepetroleum/common/items/IPItemBase.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/items/IPItemBase.java
@@ -1,10 +1,9 @@
 package flaxbeard.immersivepetroleum.common.items;
 
-import blusunrize.immersiveengineering.common.items.IEItemInterfaces.IColouredItem;
 import flaxbeard.immersivepetroleum.ImmersivePetroleum;
 import net.minecraft.world.item.Item;
 
-public class IPItemBase extends Item implements IColouredItem{
+public class IPItemBase extends Item{
 	/** For basic items */
 	public IPItemBase(){
 		this(new Item.Properties().tab(ImmersivePetroleum.creativeTab));
