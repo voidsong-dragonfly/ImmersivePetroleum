7ac7b325faf08a2857b9555494c4dfd24708c269
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2022-12-21 00:16:49 +0100

    Added ItemFrame rendering displaying almost in the same as vanilla maps.
    This is an implementation of the second point in #113

--- src/main/java/flaxbeard/immersivepetroleum/client/render/SeismicResultRenderer.java
+++ src/main/java/flaxbeard/immersivepetroleum/client/render/SeismicResultRenderer.java
@@ -4,9 +4,12 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Matrix4f;
+import com.mojang.math.Quaternion;
 
 import flaxbeard.immersivepetroleum.client.render.dyn.DynamicTextureWrapper;
+import flaxbeard.immersivepetroleum.client.utils.MCUtil;
 import flaxbeard.immersivepetroleum.common.IPContent;
+import flaxbeard.immersivepetroleum.common.items.SurveyResultItem;
 import flaxbeard.immersivepetroleum.common.util.ResourceUtils;
 import flaxbeard.immersivepetroleum.common.util.survey.ISurveyInfo;
 import flaxbeard.immersivepetroleum.common.util.survey.SurveyScan;
@@ -19,6 +22,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderHandEvent;
+import net.minecraftforge.client.event.RenderItemInFrameEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 /**
@@ -107,6 +111,71 @@ public class SeismicResultRenderer{
 						
 						buffer.endBatch();
 					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onRenderItemFrame(RenderItemInFrameEvent event){
+		if(event.getItemStack().getItem() instanceof SurveyResultItem && MCUtil.getPlayer().distanceTo(event.getItemFrameEntity()) < 1000){
+			if(ISurveyInfo.from(event.getItemStack()) instanceof SurveyScan scan){
+				DynamicTextureWrapper wrapper = DynamicTextureWrapper.getOrCreate(SurveyScan.SCAN_SIZE, SurveyScan.SCAN_SIZE, scan);
+				
+				if(wrapper != null){
+					PoseStack matrix = event.getPoseStack();
+					// MultiBufferSource buffer = event.getMultiBufferSource(); // Breaks things left and right
+					MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(TESSELATOR.getBuilder());
+					
+					matrix.pushPose();
+					{
+						int light = event.getPackedLight();
+						int rot = event.getItemFrameEntity().getRotation();
+						float scale = 1F / SurveyScan.SCAN_SIZE;
+						matrix.mulPose(new Quaternion(0, 0, 180F - -rot * 45F, true));
+						matrix.translate(-0.5F, -0.5F, -0.00625F);
+						matrix.scale(scale, scale, 1F);
+						
+						matrix.pushPose();
+						{
+							int a = wrapper.width;
+							int b = wrapper.height;
+							VertexConsumer builder = buffer.getBuffer(wrapper.renderType);
+							builder.defaultColor(255, 255, 255, 255);
+							Matrix4f mat = matrix.last().pose();
+							
+							builder.vertex(mat, 0, 0, 0).uv(1.0F, 1.0F).uv2(light).endVertex();
+							builder.vertex(mat, 0, b, 0).uv(1.0F, 0.0F).uv2(light).endVertex();
+							builder.vertex(mat, a, b, 0).uv(0.0F, 0.0F).uv2(light).endVertex();
+							builder.vertex(mat, a, 0, 0).uv(0.0F, 1.0F).uv2(light).endVertex();
+						}
+						matrix.popPose();
+						
+						// Only shows the grid, the rest is "cut off"
+						matrix.pushPose();
+						{
+							final int w = SurveyScan.SCAN_SIZE;
+							final int h = SurveyScan.SCAN_SIZE;
+							final float u = (w + 10) / 256F;
+							final float v = (h + 10) / 256F;
+							
+							matrix.translate(0, 0, -0.002F);
+							
+							VertexConsumer builder = buffer.getBuffer(RenderType.text(OVERLAY));
+							builder.defaultColor(255, 255, 255, 255);
+							Matrix4f mat = matrix.last().pose();
+							
+							builder.vertex(mat, 0, 0, 0).uv(0.0390625F, 0.0390625F).uv2(light).endVertex();
+							builder.vertex(mat, 0, h, 0).uv(0.0390625F, v).uv2(light).endVertex();
+							builder.vertex(mat, w, h, 0).uv(u, v).uv2(light).endVertex();
+							builder.vertex(mat, w, 0, 0).uv(u, 0.0390625F).uv2(light).endVertex();
+						}
+						matrix.popPose();
+					}
+					matrix.popPose();
+					
+					buffer.endBatch();
+					event.setCanceled(true);
 				}
 			}
 		}
