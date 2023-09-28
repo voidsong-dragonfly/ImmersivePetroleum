26749c2e122f4da6aeebceb9ec68993bd54180f4
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2023-09-28 06:35:05 +0200

    Remove old on-screen survey renderer

--- src/main/java/flaxbeard/immersivepetroleum/client/render/SeismicResultRenderer.java
+++ src/main/java/flaxbeard/immersivepetroleum/client/render/SeismicResultRenderer.java
@@ -13,16 +13,12 @@ import flaxbeard.immersivepetroleum.common.items.SurveyResultItem;
 import flaxbeard.immersivepetroleum.common.util.ResourceUtils;
 import flaxbeard.immersivepetroleum.common.util.survey.ISurveyInfo;
 import flaxbeard.immersivepetroleum.common.util.survey.SurveyScan;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderItemInFrameEvent;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 /**
@@ -41,85 +37,6 @@ public class SeismicResultRenderer{
 	static final Tesselator TESSELATOR = new Tesselator();
 	
 	private static final ResourceLocation OVERLAY = ResourceUtils.ip("textures/gui/seismic_overlay.png");
-	private static final ResourceLocation HOTBAR = VanillaGuiOverlay.HOTBAR.id();
-	
-	@SubscribeEvent
-	public void renderGameOverlayEvent(RenderGuiOverlayEvent.Post event){
-		boolean debug = true;
-		if(debug)
-			return;
-		
-		Minecraft mc = Minecraft.getInstance();
-		
-		if(mc.player != null && event.getOverlay().id() == HOTBAR){
-			ItemStack main = mc.player.getItemInHand(InteractionHand.MAIN_HAND);
-			ItemStack off = mc.player.getItemInHand(InteractionHand.OFF_HAND); // TODO Offhand variant?
-			
-			if((!main.isEmpty() && main.getItem() == IPContent.Items.SURVEYRESULT.get()) || (!off.isEmpty() && off.getItem() == IPContent.Items.SURVEYRESULT.get())){
-				PoseStack matrix = event.getPoseStack();
-				
-				int guiScaledWidth = event.getWindow().getGuiScaledWidth();
-				int guiScaledHeight = event.getWindow().getGuiScaledHeight();
-				
-				float xCenter = guiScaledWidth / 2F;
-				float yCenter = guiScaledHeight / 2F;
-				
-				if(ISurveyInfo.from(main) instanceof SurveyScan scan){
-					DynamicTextureWrapper wrapper = DynamicTextureWrapper.getOrCreate(SurveyScan.SCAN_SIZE, SurveyScan.SCAN_SIZE, scan);
-					
-					if(wrapper != null){
-						MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(TESSELATOR.getBuilder());
-						
-						matrix.pushPose();
-						{
-							matrix.translate(xCenter, yCenter, 0);
-							matrix.scale(2F, 2F, 2F);
-							
-							matrix.pushPose();
-							{
-								matrix.translate(-wrapper.width / 2F, -wrapper.height / 2F, 0);
-								
-								int a = wrapper.width;
-								int b = wrapper.height;
-								VertexConsumer builder = buffer.getBuffer(wrapper.renderType);
-								builder.defaultColor(255, 255, 255, 255);
-								Matrix4f mat = matrix.last().pose();
-								
-								builder.vertex(mat, 0, 0, 0).uv(1.0F, 1.0F).uv2(0xF000F0).endVertex();
-								builder.vertex(mat, 0, b, 0).uv(1.0F, 0.0F).uv2(0xF000F0).endVertex();
-								builder.vertex(mat, a, b, 0).uv(0.0F, 0.0F).uv2(0xF000F0).endVertex();
-								builder.vertex(mat, a, 0, 0).uv(0.0F, 1.0F).uv2(0xF000F0).endVertex();
-							}
-							matrix.popPose();
-							
-							matrix.pushPose();
-							{
-								final int w = 85;
-								final int h = 85;
-								final float u = w / 256F;
-								final float v = h / 256F;
-								
-								matrix.translate(-w / 2F, -h / 2F, 1);
-								
-								VertexConsumer builder = buffer.getBuffer(RenderType.text(OVERLAY));
-								builder.defaultColor(255, 255, 255, 255);
-								Matrix4f mat = matrix.last().pose();
-								
-								builder.vertex(mat, 0, 0, 0).uv(0.0F, 0.0F).uv2(0xF000F0).endVertex();
-								builder.vertex(mat, 0, h, 0).uv(0.0F, v).uv2(0xF000F0).endVertex();
-								builder.vertex(mat, w, h, 0).uv(u, v).uv2(0xF000F0).endVertex();
-								builder.vertex(mat, w, 0, 0).uv(u, 0.0F).uv2(0xF000F0).endVertex();
-							}
-							matrix.popPose();
-						}
-						matrix.popPose();
-						
-						buffer.endBatch();
-					}
-				}
-			}
-		}
-	}
 	
 	@SubscribeEvent
 	public void onRenderItemFrame(RenderItemInFrameEvent event){
