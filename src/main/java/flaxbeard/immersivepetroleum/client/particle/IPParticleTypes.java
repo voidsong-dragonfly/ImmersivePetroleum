524d3d40b3df8e51826d126585b9c40b08d9c083
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2023-10-08 22:52:02 +0200

    Remove `OnlyIn` annotation for IPParticleTypes.class

--- src/main/java/flaxbeard/immersivepetroleum/client/particle/IPParticleTypes.java
+++ src/main/java/flaxbeard/immersivepetroleum/client/particle/IPParticleTypes.java
@@ -8,11 +8,8 @@ import flaxbeard.immersivepetroleum.common.IPRegisters;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.registries.RegistryObject;
 
-@OnlyIn(Dist.CLIENT)
 public class IPParticleTypes{
 	public static final RegistryObject<SimpleParticleType> FLARE_FIRE = createBasicParticle("flare_fire", false);
 	public static final RegistryObject<ParticleType<FluidParticleData>> FLUID_SPILL = createParticleWithData("fluid_spill", FluidParticleData.DESERIALIZER, FluidParticleData.CODEC);
