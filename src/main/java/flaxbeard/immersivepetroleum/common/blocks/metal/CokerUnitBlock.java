a1175a0c39e0107ee478f9b243ea0d5aad94bb44
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2024-03-08 22:04:05 +0100

    Adding `@Deprecated` annotation to old MB Blocks

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/CokerUnitBlock.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/CokerUnitBlock.java
@@ -1,25 +1,25 @@
 package flaxbeard.immersivepetroleum.common.blocks.metal;
 
-import flaxbeard.immersivepetroleum.common.IPTileTypes;
-import flaxbeard.immersivepetroleum.common.blocks.IPMetalMultiblock;
-import flaxbeard.immersivepetroleum.common.blocks.tileentities.CokerUnitTileEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class CokerUnitBlock extends IPMetalMultiblock<CokerUnitTileEntity>{
+@Deprecated
+public class CokerUnitBlock extends Block{// extends IPMetalMultiblock<CokerUnitTileEntity>{
 	public CokerUnitBlock(){
-		super(IPTileTypes.COKER);
+		super(null);
+//		super(IPTileTypes.COKER);
 	}
 	
 	@Override
 	public boolean isLadder(BlockState state, LevelReader world, BlockPos pos, LivingEntity entity){
 		BlockEntity te = world.getBlockEntity(pos);
-		if(te instanceof CokerUnitTileEntity coker && coker.isLadder()){
-			return true;
-		}
+//		if(te instanceof CokerUnitTileEntity coker && coker.isLadder()){
+//			return true;
+//		}
 		return false;
 	}
 }
--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/HydrotreaterBlock.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/metal/HydrotreaterBlock.java
@@ -1,11 +1,11 @@
 package flaxbeard.immersivepetroleum.common.blocks.metal;
 
-import flaxbeard.immersivepetroleum.common.IPTileTypes;
-import flaxbeard.immersivepetroleum.common.blocks.IPMetalMultiblock;
-import flaxbeard.immersivepetroleum.common.blocks.tileentities.HydrotreaterTileEntity;
+import net.minecraft.world.level.block.Block;
 
-public class HydrotreaterBlock extends IPMetalMultiblock<HydrotreaterTileEntity>{
+@Deprecated
+public class HydrotreaterBlock extends Block{// extends IPMetalMultiblock<HydrotreaterTileEntity>{
 	public HydrotreaterBlock(){
-		super(IPTileTypes.TREATER);
+		super(null);
+//		super(IPTileTypes.TREATER);
 	}
 }
