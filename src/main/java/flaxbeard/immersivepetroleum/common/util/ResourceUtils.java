743ac51975a30563c8023e1991cc687cd6c2f9ba
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2024-03-09 17:19:03 +0100

    ResourceUtils received comments

--- src/main/java/flaxbeard/immersivepetroleum/common/util/ResourceUtils.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/util/ResourceUtils.java
@@ -5,25 +5,36 @@ import flaxbeard.immersivepetroleum.ImmersivePetroleum;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 
+/**
+ * Small {@link ResourceLocation}-Utility class for Frequently used namespaces
+ * 
+ * @author TwistedGate
+ */
 public class ResourceUtils{
-	public static ResourceLocation ip(String str){
-		return new ResourceLocation(ImmersivePetroleum.MODID, str);
+	/** Immersive Petroleum namespace */
+	public static ResourceLocation ip(String path){
+		return new ResourceLocation(ImmersivePetroleum.MODID, path);
 	}
 	
-	public static ResourceLocation ct(String str){
-		return new ResourceLocation("crafttweaker", str);
+	/** Immersive Engineering namespace */
+	public static ResourceLocation ie(String path){
+		return new ResourceLocation(Lib.MODID, path);
 	}
 	
-	public static ResourceLocation ie(String str){
-		return new ResourceLocation(Lib.MODID, str);
+	/** Craft Tweaker namespace */
+	@Deprecated(forRemoval = true)
+	public static ResourceLocation ct(String path){
+		return new ResourceLocation("crafttweaker", path);
 	}
 	
+	/** NeoForge namespace */
 	// TODO Remember to rename this later
-	public static ResourceLocation forge(String str){
-		return new ResourceLocation(NeoForgeVersion.MOD_ID, str);
+	public static ResourceLocation forge(String path){
+		return new ResourceLocation(NeoForgeVersion.MOD_ID, path);
 	}
 	
-	public static ResourceLocation mc(String str){
-		return new ResourceLocation(ResourceLocation.DEFAULT_NAMESPACE, str);
+	/** Minecraft namespace */
+	public static ResourceLocation mc(String path){
+		return new ResourceLocation(ResourceLocation.DEFAULT_NAMESPACE, path);
 	}
 }
