f034cdb8e3a3fd6830a91015eee4e9460b161aa0
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2025-10-20 16:52:22 +0200

    Renamed IPCreativeTabs to IPCreativeTab & Moved to Common.
    Where it belongs.

--- src/main/java/flaxbeard/immersivepetroleum/common/IPCreativeTab.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/IPCreativeTab.java
@@ -1,20 +1,18 @@
-package flaxbeard.immersivepetroleum.common.util;
+package flaxbeard.immersivepetroleum.common;
 
 import flaxbeard.immersivepetroleum.ImmersivePetroleum;
-import flaxbeard.immersivepetroleum.common.IPContent;
-import flaxbeard.immersivepetroleum.common.IPRegisters;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.RegistryObject;
 
-public class IPCreativeTabs{
+public class IPCreativeTab{
 	//@formatter:off
 	public static final RegistryObject<CreativeModeTab> MAIN_TAB = IPRegisters.registerCreativeTab(ImmersivePetroleum.MODID, () -> CreativeModeTab.builder()
 		.icon(() -> new ItemStack(IPContent.Fluids.CRUDEOIL.bucket().get()))
 		.title(Component.translatable("itemGroup." + ImmersivePetroleum.MODID))
-		.displayItems(IPCreativeTabs::fill).build()
+		.displayItems(IPCreativeTab::fill).build()
 	);
 	//@formatter:on
 	
